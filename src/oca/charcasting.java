bbd7af262c940a691695456ac49bc1464cad40f4
Matthew Ma <ym1340@nyu.edu>
2018-06-09 15:08:52 -0700

    First commit

---
+++ src/oca/charcasting.java
@@ -0,0 +1,13 @@
+package oca;
+
+public class charcasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char x = 8;
+		int y = '8';
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+}
---
+++ src/oca/privateconstructor.java
@@ -0,0 +1,13 @@
+package oca;
+class A{
+	private A() {
+		
+	}
+	private A(int a) {
+		
+	}
+}
+public class privateconstructor {
+public static void main(String args[]) {
+}
+}
